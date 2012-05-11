3da4c7da28dea65761d22cac0ec16ff929e62412
bdrhoa <bdrhoa@gmail.com>
2012-05-11 14:55:12 -0600

    Put temp files under test/data and updated paths to assume data in /usr/tmp/anttest. A sym link should be created for that.

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ grails-ant-test-app
 ===================
 
 Application to test the grails-ant plugin.
+
+Tests are in BootStrap.groovy. You may need to edit it to set some paths. 
+All file opperations happen in grails-ant-test/test/data. You may want to set sym link from /usr/tmp/antest to there.
+When done running the app:
+	rm -rf temp1
+	rm -rf temp2
+	rm newzip.zip 
--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -4,6 +4,7 @@ class BootStrap {
 	
     def init = { servletContext ->
 		
+/*
 		def planZipFile = "C:\\Users\\Brad\\Desktop\\temp\\itemtest.zip"
 		def theZipFile = new File("C:\\Users\\Brad\\Desktop\\temp\\itemtest.zip")
 		def workingDirName = "C:\\Users\\Brad\\Desktop\\temp\\temp42\\"
@@ -12,6 +13,20 @@ class BootStrap {
 		def fileNotFound = new File("C:\\Users\\Brad\\Desktop\\temp\\fileNotFound.zip")
 		def notAZip = new File("C:\\Users\\Brad\\Desktop\\temp\\Penguins.jpg")
 		def notAZipStr = "C:\\Users\\Brad_2\\Desktop\\temp\\Penguins.jpg"
+*/
+
+//create a sm link (ln -sf) from /usr/tmp/antest/ to  grails-ant-test/test/data
+//rm -rf temp1 temp2 after run
+//rm newzip.zip after run 
+
+		def planZipFile = "/usr/tmp/antest/itemtest.zip"
+		def theZipFile = new File("/usr/tmp/antest/itemtest.zip")
+		def workingDirName = "/usr/tmp/antest/temp1"
+		def workingDirName2 = "/usr/tmp/antest/temp2"
+		def workingDirName3 = "/usr/tmp/antest/temp3"
+		def fileNotFound = new File("/usr/tmp/antest/temp1/fileNotFound.zip")
+		def notAZip = new File("/usr/tmp/antest/temp1/Penguins.jpg")
+		def notAZipStr = "/usr/tmp/antest/temp1/Penguins.jpg"
 		
 		//def unzip(String zipFile,  String destDir)
 		antUtilsService.unzip(planZipFile,workingDirName)
@@ -27,14 +42,13 @@ class BootStrap {
 		
 		
 		//def zip(String destfile,String basedir)
-		antUtilsService.zip("newzip.zip",workingDirName2)
+		antUtilsService.zip("/usr/tmp/antest/newzip.zip",workingDirName2)
 		
 		//def zip(String destfile,String basedir,String includes)
-		antUtilsService.zip("newzip.zip",workingDirName2,"**/*.*")
+		antUtilsService.zip("/usr/tmp/antest/newzip.zip",workingDirName2,"**/*.*")
 		
 		//def zip(String destfile,String basedir,String includes,String excludes)
-		antUtilsService.zip("newzip.zip",workingDirName2,"**/*.*","")
-		
+		antUtilsService.zip("/usr/tmp/antest/newzip.zip",workingDirName2,"**/*.*","")
 		
 		ant.echo message: 'Hello From Ant!'
 				
